d34f740da8d80a7df3242211bedae8961b9e3df8
Crino-Knowledge <25200107139@post.usts.edu.cn>
2026-02-25 10:50:25 +0800

    Update httpPushLog.vue

--- src(pre/views/pushLog/httpPushLog.vue
+++ src(pre/views/pushLog/httpPushLog.vue
@@ -1,27 +1,37 @@
 <template>
-  <div>
-    <!--Layout布局-->
-    <el-row>
-      <el-col :span="24">
-        <el-row :gutter="20">
-          <el-col :span="3">
-            <!--搜索区域-->
-            <el-input
-              placeholder="请输入内容"
-              v-model="queryInfo.searchKey"
-              clearable
-              @clear="getListPage"
-            >
-              <el-button
-                slot="append"
-                icon="el-icon-search"
-                @click="getListPage"
-              ></el-button>
-            </el-input>
-          </el-col>
-          <el-col :span="3.5">
-            <span class="cf">客户名称：</span>
-            <el-select
+  <div class="page-container">
+    <!-- 页面头部 - 对齐告警中心样式 -->
+    <div class="page-header">
+      <div class="header-content">
+        <div class="page-icon purple">
+          <i class="el-icon-s-promotion"></i>
+        </div>
+        <div class="page-title">
+          <h2>推送日志</h2>
+          <p>查看消息推送记录</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- 搜索区域 - 重构为告警中心样式 -->
+    <div class="search-section">
+      <div class="search-content">
+        <div class="search-box">
+          <i class="el-icon-search search-icon"></i>
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.searchKey"
+            clearable
+            @clear="getListPage"
+            class="search-input"
+          >
+          </el-input>
+          <button class="search-btn" @click="getListPage">搜索</button>
+        </div>
+        
+        <div class="customer-select">
+          <span class="cf">客户名称：</span>
+          <el-select
             v-model="queryInfo.customerNo"
             filterable
             remote
@@ -29,125 +39,123 @@
             placeholder="请输入客户名称"
             @change="changeTypeList"
             :remote-method="remoteMethodCustomerNo"
-            :loading="loadingCustomerNo">
+            :loading="loadingCustomerNo"
+            class="select-input"
+          >
             <el-option
               v-for="item in customerList"
               :key="item.customerNo"
               :label="item.name"
-              :value="item.customerNo">
+              :value="item.customerNo"
+            >
             </el-option>
           </el-select>
-          </el-col>
-        </el-row>
-      </el-col>
-      <el-col :span="24">
-        <!--表格-->
+        </div>
+      </div>
+    </div>
+
+    <!-- 表格区域 -->
+    <div class="table-section">
+      <div class="table-header">
+        <span class="table-title">推送记录</span>
+        <span class="table-count">共 {{ total }} 条记录</span>
+      </div>
+
+      <div class="custom-table">
         <el-table
           :data="pageList"
           border
           @cell-dblclick="dbcopy"
+          style="width: 100%"
+          v-loading="loadingTable"
         >
-          <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
+          <el-table-column type="index" label="序号" width="80" align="center">
+            <template slot-scope="scope">
+              <span class="index-badge">{{ scope.$index + 1 }}</span>
+            </template>
+          </el-table-column>
           <el-table-column prop="pushDate" label="推送时间" align="center" width="180"></el-table-column>
           <el-table-column prop="status" label="推送状态" align="center" width="100">
-            <template slot-scope="scope" >
-              <span class="tag-js" v-if="scope.row.status">
-                  {{scope.row.status?'成功':'失败'}}
-              </span>
-              <span class="tag-js" v-if="!scope.row.status">
-                  {{scope.row.status?'成功':'失败'}}
+            <template slot-scope="scope">
+              <span class="status-tag" :class="scope.row.status ? 'status-active' : 'status-inactive'">
+                {{ scope.row.status ? '成功' : '失败' }}
               </span>
             </template>
           </el-table-column>
           <el-table-column prop="latestData" label="是否最新数据" align="center">
             <template slot-scope="scope">
-              <span class="tag-js" v-if="scope.row.latestData">
-                  {{scope.row.latestData?'是':'不是'}}
-              </span>
-              <span class="tag-js" v-if="!scope.row.latestData">
-                  {{scope.row.latestData?'是':'不是'}}
+              <span class="status-tag" :class="scope.row.latestData ? 'status-running' : 'status-pending'">
+                {{ scope.row.latestData ? '是' : '不是' }}
               </span>
             </template>
           </el-table-column>
-          <!-- <el-table-column prop="methodName" label="方法名称" align="center"></el-table-column> -->
           <el-table-column prop="httpReqUrl" label="请求地址" align="center" show-overflow-tooltip></el-table-column>
-          <el-table-column prop="httpReqHeader" label="请求头" align="center" ></el-table-column>
+          <el-table-column prop="httpReqHeader" label="请求头" align="center"></el-table-column>
           <el-table-column prop="httpReqParam" label="请求参数" align="center" show-overflow-tooltip></el-table-column>
           <el-table-column prop="httpResult" label="返回结果" align="center" show-overflow-tooltip></el-table-column>
           <el-table-column prop="errorMsg" label="错误信息" align="center" show-overflow-tooltip></el-table-column>
-          <el-table-column prop="taskNo" label="任务号" align="center" ></el-table-column>
-          <el-table-column prop="modelName" label="模型名称" align="center" ></el-table-column>
+          <el-table-column prop="taskNo" label="任务号" align="center"></el-table-column>
+          <el-table-column prop="modelName" label="模型名称" align="center"></el-table-column>
           <el-table-column prop="customerName" label="客户名称" align="center"></el-table-column>
-          <el-table-column label="操作" fixed="right" width="200">
+          <el-table-column label="操作" fixed="right" width="100" align="center">
             <!-- 作用域插槽 -->
             <template slot-scope="scope">
-              <!--修改按钮-->
-              <el-button
-                type="success"
-                size="mini"
-                @click="pushLog(scope.row)"
-              >推送</el-button>
-              <el-button
-              v-if="false"
-                type="primary"
-                size="mini"
-                icon="el-icon-edit"
-                @click="showEditDialog(scope.row)"
-              ></el-button>
-              <!--删除按钮-->
-              <el-button
-              v-if="false"
-                type="danger"
-                size="mini"
-                icon="el-icon-delete"
-                @click="deleteById(scope.row.id)"
-              ></el-button>
+              <div class="action-btns">
+                <button class="action-icon edit" @click="pushLog(scope.row)" title="推送">
+                  <i class="el-icon-send"></i>
+                </button>
+              </div>
             </template>
           </el-table-column>
         </el-table>
-      </el-col>
-    </el-row>
-    <el-row>
-      <!--分页区域-->
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pageNum"
-        :page-sizes="[1, 2, 5, 10]"
-        :page-size="queryInfo.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      >
-      </el-pagination>
-    </el-row>
+      </div>
+
+      <!-- 分页 -->
+      <div class="pagination-wrapper">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pageNum"
+          :page-sizes="[10, 20, 50, 100]"
+          :page-size="queryInfo.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          background
+        >
+        </el-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { parseTime } from '@/utils/ruoyi'
-import { deleteById, getById,listPage,againPushLog} from "@/api/httpPushLog";
-import { listPage as getCustomerListPage} from "@/api/customer";
+import { deleteById, getById, listPage, againPushLog } from "@/api/httpPushLog";
+import { listPage as getCustomerListPage } from "@/api/customer";
 import Clipboard from "clipboard"
+
 export default {
   data() {
     return {
       pageList: [], // 列表
       total: 0, // 总数
+      loadingTable: false, // 表格加载状态
       // 获取列表的参数对象
       queryInfo: {
         searchKey: "", // 查询参数
         pageNum: 1, // 当前页码
         pageSize: 10, //页面大小
+        customerNo: "" // 客户编号
       },
       addDialogVisible: false, //控制-添加对象对话框-是否一进页面就显示
-  
       editDialogVisible: false, // 控制-修改对象对话框-是否一进页面显示
       multipleSelection: [],
       ids: [],
       fileList: [],
-      typelist:[{id:1,value:"吸烟"},{id:2,value:"安全帽"},{id:3,value:"人脸"}],
-      customerList:[],
-      loadingCustomerNo:false,
+      typelist: [{ id: 1, value: "吸烟" }, { id: 2, value: "安全帽" }, { id: 3, value: "人脸" }],
+      customerList: [],
+      loadingCustomerNo: false,
+      isLoaddingLog: false, // 推送加载状态
       queryCustomerInfo: {
         searchKey: "", // 查询参数
         pageNum: 1, // 当前页码
@@ -157,11 +165,10 @@ export default {
   },
   created() {
     // 生命周期函数
-    
     this.getCustomerList()
   },
   methods: {
-    dbcopy(row, column, cell, event){
+    dbcopy(row, column, cell, event) {
       this.handleClipboard(event)
     },
     handleClipboard(event) {
@@ -186,50 +193,56 @@ export default {
       })
       clipboard.onClick(event)
     },
-    changeTypeList(){
+    changeTypeList() {
       this.queryInfo.pageNum = 1;
       // 重新发起请求列表
       this.getListPage();
     },
-    getCustomerList(){
+    getCustomerList() {
       this.loadingCustomerNo = true
-      getCustomerListPage(this.queryCustomerInfo).then(res=>{
+      getCustomerListPage(this.queryCustomerInfo).then(res => {
         this.loadingCustomerNo = false
         if (res.data.code === 200) {
-            this.customerList = res.data.data.list;
+          this.customerList = res.data.data.list;
+          if (this.customerList.length > 0) {
             this.queryInfo.customerNo = this.customerList[0].customerNo
-            this.changeTypeList();
+          }
+          this.getListPage();
         }
-      }).catch(err=>{
+      }).catch(err => {
         this.loadingCustomerNo = false
       })
     },
-    remoteMethodCustomerNo(query){
-      if(query != ''){
+    remoteMethodCustomerNo(query) {
+      if (query != '') {
         this.queryCustomerInfo.searchKey = query
         this.getCustomerList()
       }
     },
-    pushLog(row){
+    pushLog(row) {
       this.isLoaddingLog = true
       againPushLog({
-        httpPushLogId:row.id
-      }).then(res=>{
+        httpPushLogId: row.id
+      }).then(res => {
         this.isLoaddingLog = false
         if (res.data.code === 200) {
           this.$message.success('推送成功');
+          this.getListPage(); // 推送成功后刷新列表
         }
-      }).catch(err=>{
+      }).catch(err => {
         this.isLoaddingLog = false
+        this.$message.error('推送失败');
       })
     },
     //默认显示时分秒，此处传入pattern {y}-{m}-{d}即只显示年月日
     parseTime(timestamp) {
-      return parseTime(timestamp,"{y}-{m}-{d} {h}:{i}:{s}");
+      return parseTime(timestamp, "{y}-{m}-{d} {h}:{i}:{s}");
     },
     getListPage() {
+      this.loadingTable = true
       listPage(this.queryInfo)
         .then((res) => {
+          this.loadingTable = false
           if (res.data.code === 200) {
             this.pageList = res.data.data.list;
             this.total = res.data.data.total;
@@ -238,19 +251,18 @@ export default {
           }
         })
         .catch((err) => {
+          this.loadingTable = false
           console.log(err);
         });
     },
     // 监听 pageSize 改变的事件
     handleSizeChange(newSize) {
-      // console.log(newSize)
       this.queryInfo.pageSize = newSize;
       // 重新发起请求列表
       this.getListPage();
     },
     // 监听 当前页码值 改变的事件
     handleCurrentChange(newPage) {
-      // console.log(newPage)
       this.queryInfo.pageNum = newPage;
       // 重新发起请求列表
       this.getListPage();
@@ -285,7 +297,6 @@ export default {
     // 监听修改状态
     showEditDialog(obj) {
       this.editDialogVisible = true;
-      //console.log("请求后接收到的响应结果:"+obj);
       this.editForm = obj;
     },
     //修改
@@ -320,9 +331,7 @@ export default {
           type: "warning",
         }
       ).catch((err) => err);
-      // 如果用户确认删除，则返回值为字符串 confirm
-      // 如果用户取消删除，则返回值为字符串 cancel
-      // console.log(confirmResult)
+      
       if (confirmResult == "confirm") {
         //删除
         deleteById(id)
@@ -347,15 +356,286 @@ export default {
 };
 </script>
 
-<style>
-.el-row {
+<style scoped>
+/* 页面容器 */
+.page-container {
+  padding: 0;
+}
+
+/* 页面头部 */
+.page-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+}
+
+.header-content {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
+.page-icon {
+  width: 56px;
+  height: 56px;
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 31px;
+  color: #ffffff;
+}
+
+.page-icon.purple {
+  background: linear-gradient(135deg, #8b5cf6 0%, #7c3aed 100%);
+}
+
+.page-title h2 {
+  font-size: 27px;
+  font-weight: 700;
+  color: #1e293b;
+  margin: 0 0 4px 0;
+}
+
+.page-title p {
+  font-size: 17px;
+  color: #94a3b8;
+  margin: 0;
+}
+
+/* 搜索区域 */
+.search-section {
+  background: #faf8f0;
+  border-radius: 16px;
+  padding: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.05);
+}
+
+.search-content {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+.search-box {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  background: #f8fafc;
+  border-radius: 12px;
+  padding: 4px;
+  border: 2px solid #e2e8f0;
+  transition: all 0.3s ease;
+  max-width: 400px;
+}
+
+.search-box:focus-within {
+  border-color: #667eea;
+  background: #ffffff;
+}
+
+.search-icon {
+  font-size: 21px;
+  color: #94a3b8;
+  margin-left: 12px;
+}
+
+.search-input {
+  flex: 1;
+}
+
+.search-input >>> .el-input__inner {
+  border: none;
+  background: transparent;
+  height: 40px;
+  font-size: 17px;
+  color: #1e293b;
+}
+
+.search-btn {
+  height: 40px;
+  padding: 0 20px;
+  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  color: #ffffff;
+  border: none;
+  border-radius: 10px;
+  font-size: 17px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.search-btn:hover {
+  transform: translateY(-1px);
+  box-shadow: 0 4px 12px rgba(102, 126, 234, 0.3);
+}
+
+.customer-select {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.customer-select .cf {
+  font-size: 17px;
+  color: #1e293b;
+  font-weight: 500;
+}
+
+.select-input >>> .el-select__wrapper {
+  height: 40px;
+  font-size: 17px;
+  border-radius: 10px;
+  border: 2px solid #e2e8f0;
+}
+
+.select-input >>> .el-input__inner {
+  font-size: 17px;
+}
+
+/* 表格区域 */
+.table-section {
+  background: #faf8f0;
+  border-radius: 16px;
+  padding: 24px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.05);
+}
+
+.table-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin-bottom: 20px;
 }
-.el-col {
-  border-radius: 4px;
+
+.table-title {
+  font-size: 21px;
+  font-weight: 600;
+  color: #1e293b;
 }
-/* .el-card {
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1) !important;
-  height: 60pt;
-} */
+
+.table-count {
+  color: #94a3b8;
+  font-size: 16px;
+}
+
+.custom-table >>> .el-table {
+  background: transparent;
+}
+
+.custom-table >>> .el-table th {
+  background: #f8fafc;
+  color: #64748b;
+  font-weight: 600;
+  font-size: 16px;
+}
+
+.custom-table >>> .el-table td {
+  font-size: 17px;
+  color: #475569;
+}
+
+.custom-table >>> .el-table--enable-row-hover .el-table__body tr:hover > td {
+  background: #f8fafc;
+}
+
+.index-badge {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+  background: #f1f5f9;
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #64748b;
+}
+
+.status-tag {
+  display: inline-flex;
+  align-items: center;
+  padding: 4px 12px;
+  border-radius: 20px;
+  font-size: 15px;
+  font-weight: 500;
+}
+
+.status-active {
+  background: #dcfce7;
+  color: #166534;
+}
+
+.status-inactive {
+  background: #fee2e2;
+  color: #991b1b;
+}
+
+.status-running {
+  background: #dbeafe;
+  color: #1e40af;
+}
+
+.status-pending {
+  background: #fef3c7;
+  color: #92400e;
+}
+
+.action-btns {
+  display: flex;
+  gap: 8px;
+  justify-content: center;
+}
+
+.action-icon {
+  width: 32px;
+  height: 32px;
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 17px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.action-icon.edit {
+  background: #dbeafe;
+  color: #2563eb;
+}
+
+.action-icon.edit:hover {
+  background: #2563eb;
+  color: #ffffff;
+}
+
+/* 分页 */
+.pagination-wrapper {
+  margin-top: 24px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
+
+<!-- 全局放大Element UI组件内置文字 -->
+<style>
+.el-pagination, .el-dialog, .el-form, .el-descriptions {
+  font-size: 17px !important;
+}
+.el-descriptions-item__label, .el-descriptions-item__content {
+  font-size: 17px !important;
+}
+.el-dialog__title {
+  font-size: 20px !important;
+}
+.el-form-item__label, .el-input__inner, .el-button, .el-select {
+  font-size: 17px !important;
+}
+.el-date-picker {
+  font-size: 17px !important;
+}
 </style>
